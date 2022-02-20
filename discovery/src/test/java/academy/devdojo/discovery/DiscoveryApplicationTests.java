db8b9db6a56601d4bc7d9b630d092400a273e0b4
unknown <israelferreira5@outlook.com>
2022-02-20 00:29:54 -0300

    Add Service Discovery with Netflix Eureka and Zuul.

--- discovery/src/test/java/academy/devdojo/discovery/DiscoveryApplicationTests.java
+++ discovery/src/test/java/academy/devdojo/discovery/DiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package academy.devdojo.course;
+package academy.devdojo.discovery;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CourseApplicationTests {
+public class DiscoveryApplicationTests {
 
 	@Test
 	public void contextLoads() {
